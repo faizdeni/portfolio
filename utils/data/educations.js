c9dce44018940b4d63c5c2cf0ed052d6f2333d0f
Faiz Deni <faizdeni84@gmail.com>
2026-03-01 02:34:48 +0800

    Fix typo in institution name for Bachelor of Information Technology entry

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,7 +3,7 @@ export const educations = [
     id: 1,
     title: "Bachelor of Information Technology (Hons.) in Software Engineering",
     duration: "September 2022 - February 2026",
-    institution: "Univeristi Kuala Lumpur Malaysian Institute of Information Technology (UniKL MIIT)",
+    institution: "Universiti Kuala Lumpur Malaysian Institute of Information Technology (UniKL MIIT)",
   },
   {
     id: 2,
